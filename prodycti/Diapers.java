8d693e2eccf53c7323b584410028be279842417a
NikitaKovalev <nik.kovalev.00@bk.ru>
2023-03-09 19:27:17 +0300

    folder prodycti added diapers

---
+++ prodycti/Diapers.java
@@ -0,0 +1,30 @@
+package prodycti;
+
+import prodycti.grupe.classi.product;
+
+public class Diapers extends Hygiene {
+    String size;
+    double minWeight;
+    double maxWeight;
+    String type;
+
+    public Diapers(String name, Double price, Double quantity, String unit, int piecesInPackaging, String size,
+            double minWeight, double maxWeight, String type) {
+        super(name, price, quantity, unit, piecesInPackaging);
+        this.size = size;
+        this.minWeight = minWeight;
+        this.maxWeight = maxWeight;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Diapers: " + super.toString());
+        sb.append(String.format("Size: %s  ", this.size));
+        sb.append(String.format("Minimal weight: %.2f  ", this.minWeight));
+        sb.append(String.format("maximal weight: %.2f  ", this.maxWeight));
+        sb.append(String.format("Type: %s  ", this.type));
+        return sb.toString();
+    }
+}
+}
